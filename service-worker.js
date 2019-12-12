ebf5de4525984237d9d1507b9baaf861999f0135
Arham Ali Qureshi <aqureshi030@me.ema.ad.pwcinternal.com>
2019-12-12 06:01:15 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/idesBoard/precache-manifest.78ee8092c52052f3fd3e129617a1ffac.js"
+  "/ideasBoard/precache-manifest.11a8648b5e60bcbd2e90d52f7472505c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/idesBoard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ideasBoard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
