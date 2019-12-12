77fd1d5cca9ec6c76dffaefa1831aa960a1ac1c1
Arham Ali Qureshi <aqureshi030@me.ema.ad.pwcinternal.com>
2019-12-12 06:48:48 +0400

    Logo updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,13 +4,17 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 
 
-const  NavBar = () =>{
+
+const NavBar = () => {
     return (<div>
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="title" color="inherit">
-                    Coolshop Ideas Sharing Board
+
+                <img src={"https://www.coolshop.it/src/images/logo_coolshop_72_white.png"} alt="Logo" width="124.22" height="30"></img>
+                <Typography variant="h5" component='h2' color="inherit" align="center" display="block" style={{paddingLeft:"5%"}}>
+                    Ideas Sharing Board
                 </Typography>
+
             </Toolbar>
         </AppBar>
     </div>);
